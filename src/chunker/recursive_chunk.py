ac0e0b0c243d1fc0074c8d09d762f5efe025c79c
luannt1010 <luanntai13@gmail.com>
2026-09-14 15:34:33 +0700

    add recursive chunk

---
+++ src/chunker/recursive_chunk.py
@@ -0,0 +1,26 @@
+from .base import BaseChunker
+from langchain_core.documents import Document
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+SEPARATOR = [
+        "\n#{1,6} ",
+        "```\n",
+        "\n\*\*\*+\n",
+        "\n---+\n",
+        "\n___+\n",
+        "\n\n",
+        "\n",
+        " ",
+        "",
+    ]
+
+class RecursiveChunk(BaseChunker):
+    def __init__(self, chunk_size: int, chunk_overlap: int):
+        self.splitter = RecursiveCharacterTextSplitter(
+            chunk_size=chunk_size,
+            chunk_overlap=chunk_overlap,
+            add_start_index=True,
+            separators=SEPARATOR)
+
+    def split(self, documents: list[Document]) -> list[Document]:
+        return self.splitter.split_documents(documents)
