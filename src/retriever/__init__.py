0ae91b49a283293b3c32c931db1e68b04b698648
luannt1010 <luanntai13@gmail.com>
2026-09-22 09:34:42 +0700

    update init

--- src/retriever/__init__.py
+++ src/retriever/__init__.py
@@ -1,5 +1,6 @@
+from .base import BaseRetriever
 from .bm25 import BM25
 from .dense import DenseRetriever
 from .hybrid import HybridRetriever
 
-__all__ = ["BM25", "DenseRetriever", "HybridRetriever"]
+__all__ = ["BaseRetriever", "BM25", "DenseRetriever", "HybridRetriever"]
